811e6c24aec261a41a1a7ab26e8ba3910e45cb83
agjunyent <albertgaju@gmail.com>
2025-04-15 00:53:54 +0200

    Fixed laserscan qos and frame_id

--- sllidar_ros2/src/sllidar_node.cpp
+++ sllidar_ros2/src/sllidar_node.cpp
@@ -59,7 +59,7 @@ class SLlidarNode : public rclcpp::Node
     : Node("sllidar_node")
     {
 
-      scan_pub = this->create_publisher<sensor_msgs::msg::LaserScan>("scan", rclcpp::QoS(rclcpp::KeepLast(10)));
+      scan_pub = this->create_publisher<sensor_msgs::msg::LaserScan>("scan", rclcpp::SensorDataQoS());
       
     }
 
